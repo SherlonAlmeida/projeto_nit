cadcdfa1fa21903453e88e2cbf28f6e9223d983a
SherlonAlmeida <sherlonalmeidadasilva@gmail.com>
2023-06-02 21:06:24 -0300

    Cadastro de usuários implementado

--- menu.py
+++ menu.py
@@ -1,3 +1,19 @@
-print('Oi')
+import sqlite3 # importa a pasta SQL  para dentro do python
 
-#Sherlon disse oi pro Marcelo
+conexao = sqlite3.connect("gestaoNIT.sqlite3") # Conecta o banco de dados com Python
+cursor = conexao.cursor() # cursor para executar comandos SQL, tanto os DML como os DDL
+##################################################
+
+tipo = input("Digite o tipo de usuário: ")
+nome = input("Digite o seu nome: ")
+email = input("Digite o seu email: ")
+senha = input("Digite a sua senha: ")
+
+comando_sql = """INSERT INTO Usuarios (tipo, nome, email, senha) VALUES (?,?,?,?)"""
+valores = [tipo, nome, email, senha]
+cursor.execute(comando_sql, valores)
+
+##################################################
+conexao.commit() # A função “commit”, associada à variável “conexao”, chamada logo em seguida, 
+                 # serve para efetivamente salvar as alterações realizadas no banco de dados. 
+conexao.close()  # é chamada para fechar a conexão
